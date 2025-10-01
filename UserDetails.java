31d075f982befceb6d697cad091ce7b971f3ecea
Vinod2050 <vinodmache7@gmail.com>
2025-10-01 11:55:12 +0530

    Initial project commit

---
+++ UserDetails.java
@@ -0,0 +1,17 @@
+// Basic Java program to print user details
+public class UserDetails {
+    public static void main(String[] args) {
+        // User details
+        String name = "Vinod Mache";
+        int age = 28;
+        String city = "Pune";
+        String email = "vinod.mache@outlook.com";
+
+        // Printing user details
+        System.out.println("=== User Details ===");
+        System.out.println("Name   : " + name);
+        System.out.println("Age    : " + age);
+        System.out.println("City   : " + city);
+        System.out.println("Email  : " + email);
+    }
+}
